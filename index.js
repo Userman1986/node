2e27f34eb284bcb7ad77215074cd372c7b00d02e
Userman1986 <e.samonin1986@gmail.com>
2023-06-21 17:20:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,44 +2,29 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 
+const movies = [
+  { title: 'Movie 1', rating: 8 },
+  { title: 'Movie 2', rating: 7 },
+  // Add more movie 
+];
+
 app.use(express.static('public'));
 app.use(morgan('dev'));
 
 app.use((err, req, res, next) => {
-    console.error(err);
-    res.status(500).send('Internal Server Error');
-  });
-
-  app.get('/movies', (req, res, next) => {
-    // Some code that may throw an error
-    try {
-      // ...
-    } catch (err) {
-      next(err);
-    }
-  });
-  
-
+  console.error(err);
+  res.status(500).send('Internal Server Error');
+});
 
 app.get('/movies', (req, res) => {
-    const movies = [
-      { title: 'Movie 1', rating: 9 },
-      { title: 'Movie 2', rating: 9 },
-      // Add more movie objects as needed
-    ];
-    res.json(movies);
-  });
-  
-  app.get('/', (req, res) => {
-    res.send('Welcome to my movie API!');
-  });
-  
+  res.json(movies);
+});
 
+app.get('/', (req, res) => {
+  res.send('Welcome to my movie API!');
+});
 
-
-
-
-  const port = 1800;
+const port = 1800;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });

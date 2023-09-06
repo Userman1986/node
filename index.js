976566ce34ab1d7c6fd026bfffea91a2af80e009
Userman1986 <e.samonin1986@gmail.com>
2023-09-06 10:19:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const { Schema } = mongoose;
 const { Movie, Genre, Director, User } = require('./models');
 const passport = require('passport');
 
+
 mongoose.connect('mongodb+srv://esamonin1986:greenfly@mymovieapi.lxey35j.mongodb.net/MyMovieApi?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('Connected to MongoDB');
@@ -21,14 +22,24 @@ app.use(express.json());
 require('./auth')(app);
 const cors = require('cors');
 
+
+const corsOptions = {
+  origin: ['http://localhost:1234', 'http://testsite.com', 'https://guarded-hamlet-46049-f301c8b926bd.herokuapp.com'],
+};
+
+
 let allowedOrigins = ['http://localhost:1234', 'http://testsite.com', 'https://guarded-hamlet-46049-f301c8b926bd.herokuapp.com'];
+
+
+app.use(cors(corsOptions));
+
 
 app.use(cors({
   origin: (origin, callback) => {
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn't found on the list of allowed origins
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
       let message = "The CORS policy for this application doesn't allow access from origin " + origin;
-      return callback(new Error(message ), false);
+      return callback(new Error(message), false);
     }
     return callback(null, true);
   }
